1d37d8567c56f8ebfe84c9a5568a2369445c1e40
Владислав Балобанов <balobasta@gmail.com>
2021-01-25 15:31:25 +0200

    feat(controllers)[PlacesController]: add comments

--- app/controllers/place/getPlacesByFields.go
+++ app/controllers/place/getPlacesByFields.go
@@ -4,5 +4,7 @@ import "../../../models"
 
 func GetPlacesByFields(fieldsNames []string, searchedValue string) (models.Place, error)  {
 
+	//ищем места где содержимое полей похоже на строку поиска
+
 	return models.Place{}, nil
 }
--- app/controllers/place/getPlacesByTag.go
+++ app/controllers/place/getPlacesByTag.go
@@ -4,5 +4,10 @@ import "../../../models"
 
 func GetPlacesByTags(tagsUIDs []models.UID) ([]models.Place, error) {
 
+	//ищем похожие теги
+
+	//по тегам ищем места
+
+
 	return nil, nil
 }
