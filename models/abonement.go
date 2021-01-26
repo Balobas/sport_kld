e574aea9986772ea4f12d5a484c02476d4510bfa
Владислав Балобанов <balobasta@gmail.com>
2021-01-26 19:17:03 +0200

    refactor(models)[models]: delete core directory, delete interfaces and Object actions

--- models/abonement.go
+++ models/abonement.go
@@ -1,9 +1,6 @@
 package models
 
 import (
-	"../app/utils"
-	"../core"
-	"encoding/json"
 	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"
 )
@@ -21,72 +18,15 @@ type Abonement struct {
 }
 
 //keys
-func (abonement Abonement) GenerateAndSetKey() (core.Object, error) {
+func (abonement Abonement) GenerateAndSetKey() error {
 	uid, err := uuid.NewV4()
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return errors.WithStack(err)
 	}
 	abonement.UID = UID(uid.String())
-	return abonement, nil
+	return nil
 }
 
-func (abonement Abonement) GetKey() string {
-	return abonement.UID.String()
-}
-
-func (abonement Abonement) SetKey(key string) (core.Object, error) {
-	if !(UID(key)).isCorrect() {
-		return nil, errors.Errorf("uid is not correct %s", key)
-	}
-	abonement.UID = UID(key)
-	return abonement, nil
-}
-
-//
-
-//new
-func (abonement Abonement) New() core.Object {
-	return Abonement{
-		UID:            "",
-		Name:           "",
-		Description:    "",
-		Price:          "",
-		ActivateTime:   "",
-		PlacesUIDs:     nil,
-		CategoriesUIDs: nil,
-	}
-}
-
-//
-
-//unmarshal
-func (abonement Abonement) UnmarshalFromMap(objMap map[string]interface{}) (core.Object, error) {
-	if err := utils.UnmarshalFromMap(&abonement, objMap); err != nil {
-		return nil, errors.WithStack(err)
-	}
-	return abonement, nil
-}
-
-//
-
-//update
-func (abonement Abonement) Update(updateAbonementInterface interface{}) (core.Object, error) {
-	var updateAbonement Abonement
-	b, err := json.Marshal(updateAbonementInterface)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	if err := json.Unmarshal(b, &updateAbonement); err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	//обновление полей, которые можно обновить, ошибки если нельзя обновить
-	abonement = updateAbonement
-
-	return abonement, nil
-}
-
-//
 
 //validations
 func (abonement Abonement) Validate() error {
--- models/organization.go
+++ models/organization.go
@@ -1,9 +1,6 @@
 package models
 
 import (
-	"../app/utils"
-	"../core"
-	"encoding/json"
 	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"
 )
@@ -17,67 +14,14 @@ type Organization struct {
 }
 
 //keys
-func (org Organization) GenerateAndSetKey() (core.Object, error) {
+func (org *Organization) GenerateAndSetKey() error {
 	uid, err := uuid.NewV4()
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return errors.WithStack(err)
 	}
 	org.UID = UID(uid.String())
-	return org, nil
+	return nil
 }
-
-func (org Organization) GetKey() string {
-	return org.UID.String()
-}
-
-func (org Organization) SetKey(key string) (core.Object, error) {
-	if !(UID(key)).isCorrect() {
-		return nil, errors.Errorf("uid is not correct %s", key)
-	}
-	org.UID = UID(key)
-	return org, nil
-}
-
-//
-
-//new
-func (org Organization) New() core.Object {
-	return Organization{
-		UID:         "",
-		Name:        "",
-		Description: "",
-	}
-}
-
-//
-
-//unmarshal
-func (org Organization) UnmarshalFromMap(objMap map[string]interface{}) (core.Object, error) {
-	if err := utils.UnmarshalFromMap(&org, objMap); err != nil {
-		return nil, errors.WithStack(err)
-	}
-	return org, nil
-}
-
-//
-
-//update
-func (org Organization) Update(updateOrgInterface interface{}) (core.Object, error) {
-	var updateOrg Organization
-	b, err := json.Marshal(updateOrgInterface)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	if err := json.Unmarshal(b, &updateOrg); err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	//обновление полей, которые можно обновить, ошибки если нельзя обновить
-	org = updateOrg
-
-	return org, nil
-}
-
 //
 
 //validations
