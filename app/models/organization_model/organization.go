8ba9cacded1c2921cc50e6e44d5444a883c02cc6
Владислав Балобанов <balobasta@gmail.com>
2021-02-05 16:40:18 +0200

    feat(organization model)[models]: delete deprecated methods

--- app/models/organization_model/organization.go
+++ app/models/organization_model/organization.go
@@ -2,8 +2,6 @@ package organization_model
 
 import (
 	"../../models"
-	"github.com/pkg/errors"
-	uuid "github.com/satori/go.uuid"
 )
 
 type Organization struct {
@@ -11,26 +9,3 @@ type Organization struct {
 	Name        string `json:"name" db:"name"`
 	Description string `json:"description" db:"description"`
 }
-
-//keys
-func (org *Organization) GenerateAndSetKey() error {
-	uid, err := uuid.NewV4()
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	org.UID = models.UID(uid.String())
-	return nil
-}
-//
-
-//validations
-func (org Organization) Validate() error {
-
-	return nil
-}
-
-func (org Organization) IsValidUID() bool {
-	return len(org.UID) != 0
-}
-
-//
