01f24f9d589357f3be7f75cec47157d7cfafae32
Владислав Балобанов <balobasta@gmail.com>
2021-02-07 16:40:17 +0200

    feat(place model tests)[tests]: add GetPlacesByFields test

---
+++ app/models/place_model/getPlacesByFields_test.go
@@ -0,0 +1,50 @@
+package place_model
+
+import (
+	"../place_model"
+	"testing"
+)
+
+func TestGetPlacesByFields(t *testing.T) {
+	// Поиск по одному полю.
+	searchString := "Альбатрос"
+	fieldsNames := []string{"name"}
+
+	places, errors := place_model.GetPlacesByFields(fieldsNames, searchString)
+	if errors != nil {
+		t.Log(errors)
+		t.Log("FAIL: cant select place by on field")
+		t.Fail()
+	} else {
+		//t.Log(places)
+		t.Log("select places by one field was successful\n", "found ", len(places), " places")
+	}
+
+	// Поиск по нескольким полям.
+	searchString = "Баскетбол"
+	fieldsNames = []string{"name", "description"}
+
+	places, errors = place_model.GetPlacesByFields(fieldsNames, searchString)
+	if errors != nil {
+		t.Log(errors)
+		t.Log("FAIL: cant select places by two fields")
+		t.Fail()
+	} else {
+		//t.Log(places)
+		t.Log("select places by one two fields was successful\n", "found ", len(places), " places")
+	}
+
+	// Поиск без указания поля.
+	searchString = "Футбол"
+	fieldsNames = []string{}
+
+	places, errors = place_model.GetPlacesByFields(fieldsNames, searchString)
+	if errors != nil {
+		t.Log(errors)
+		t.Log("FAIL: cant select places by empty fields array")
+		t.Fail()
+	} else {
+		//t.Log(places)
+		t.Log("select places by empty fields array was successful\n", "found ", len(places), " places")
+	}
+}
