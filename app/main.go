17c8e476ddd1dee1a3e4799f51a7ccb218971e12
Владислав Балобанов <balobasta@gmail.com>
2021-02-08 18:09:27 +0200

    feat(main.go)[API]: add http handle func for GetOrganizationPlaces

--- app/main.go
+++ app/main.go
@@ -9,7 +9,7 @@ func main() {
 	http.HandleFunc("/place", api.GetPlaceByUID)
 	http.HandleFunc("/places", api.GetPlacesByUIDs)
 	http.HandleFunc("/places_by_fields", api.GetPlacesByFields)
-
+	http.HandleFunc("/organization_places", api.GetOrganizationPlaces)
 
 	http.ListenAndServe(":8095", nil)
 }
