2f9599ef8406b0492a62d671abadc9a0b22ea90a
Владислав Балобанов <balobasta@gmail.com>
2021-02-08 18:11:40 +0200

    feat(places controller)[]: add GetPlacesByOrganizationUID method

--- app/controllers/place_controller/getPlacesByOrganizationUid.go
+++ app/controllers/place_controller/getPlacesByOrganizationUid.go
@@ -1,1 +1,11 @@
 package place_controller
+
+import (
+	"../../models"
+	"../../models/place_model"
+)
+
+
+func GetPlacesByOrganizationUID(organizationUid string) ([]place_model.Place, []error) {
+	return place_model.GetPlacesByOrganizationUID(models.UID(organizationUid))
+}
