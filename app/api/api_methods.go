0634ffba02ad3bf020b83d5bc44a3760999e2dff
Владислав Балобанов <balobasta@gmail.com>
2021-02-08 15:39:38 +0200

    feat(api)[API]: add place api methods signature

--- app/api/api_methods.go
+++ app/api/api_methods.go
@@ -6,6 +6,24 @@ package api
 
 import "net/http"
 
-func GetPlaceInfoByUID(w http.ResponseWriter, r *http.Request) {
+func GetPlaceByUID(w http.ResponseWriter, r *http.Request) {
 
 }
+
+func GetPlacesByUIDs(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetPlacesByFields(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetOrganizationPlaces(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetPlacesByTag(w http.ResponseWriter, r *http.Request) {
+
+}
+
+
