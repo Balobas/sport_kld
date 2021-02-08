46935a4bc7671f2c8ddc52628095e4898c17d32e
Владислав Балобанов <balobasta@gmail.com>
2021-02-08 17:29:31 +0200

    feat(main.go)[API]: add handlers and listen and serve func

--- app/main.go
+++ app/main.go
@@ -1,6 +1,14 @@
 package main
 
-func main() {
+import (
+	"../app/api"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/place", api.GetPlaceByUID)
+	http.HandleFunc("/places", api.GetPlacesByUIDs)
+
+	http.ListenAndServe(":8095", nil)
 }
 
