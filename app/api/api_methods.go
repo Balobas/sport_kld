5ec956bebd652bf1a734a6e4a93409177be61501
Владислав Балобанов <balobasta@gmail.com>
2021-02-08 17:17:54 +0200

    feat(place api)[API]: add GetPlaceByUID handler, GET /place?uid=place_uid

--- app/api/api_methods.go
+++ app/api/api_methods.go
@@ -4,15 +4,53 @@
  */
 package api
 
-import "net/http"
+import (
+	"../controllers/place_controller"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func GetPlaceByUID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		res := []byte("wrong http method. access denied")
+		if _, err := w.Write(res); err != nil {
+			fmt.Println("cant write bytes")
+		}
+		return
+	}
 
+	if r.URL.Query().Get("uid") == "" {
+		if _, err := w.Write([]byte("not found uid key in get query")); err != nil {
+			fmt.Println("cant write bytes")
+		}
+		return
+	}
+
+	uid := r.URL.Query()["uid"][0]
+
+	place, err := place_controller.GetPlaceByUID(uid)
+	if err != nil {
+		res := []byte(err.Error())
+		if _, err := w.Write(res); err != nil {
+			fmt.Println("cant write bytes")
+		}
+		return
+	}
+	b, err := json.Marshal(place)
+	if err != nil {
+		res := []byte(err.Error())
+		if _, err := w.Write(res); err != nil {
+			fmt.Println("cant write bytes")
+		}
+		return
+	}
+	if _, err := w.Write(b); err != nil {
+		fmt.Println("cant write bytes")
+	}
 }
 
-func GetPlacesByUIDs(w http.ResponseWriter, r *http.Request) {
-
-}
+func GetPlacesByUIDs(w http.ResponseWriter, r *http.Request) {}
 
 func GetPlacesByFields(w http.ResponseWriter, r *http.Request) {
 
