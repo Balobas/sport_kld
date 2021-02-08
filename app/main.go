5cd7039ceea47555731a1f5b720e4ee2cbfeb110
Владислав Балобанов <balobasta@gmail.com>
2021-02-08 17:48:13 +0200

    feat(main.go)[API]: add http handler /places_by_fields?search=search_string&fields=field1&fields=field2

--- app/main.go
+++ app/main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	http.HandleFunc("/place", api.GetPlaceByUID)
 	http.HandleFunc("/places", api.GetPlacesByUIDs)
+	http.HandleFunc("/places_by_fields", api.GetPlacesByFields)
+
 
 	http.ListenAndServe(":8095", nil)
 }
