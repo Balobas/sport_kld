414adbd7448953429ed9478f9df96aaed892b9a7
Владислав Балобанов <balobasta@gmail.com>
2021-02-10 15:10:00 +0200

    feat(main.go): add http handler func for '/' and '/favicon.ico' patterns

--- app/main.go
+++ app/main.go
@@ -10,6 +10,8 @@ func main() {
 	http.HandleFunc("/places", api.GetPlacesByUIDs)
 	http.HandleFunc("/places_by_fields", api.GetPlacesByFields)
 	http.HandleFunc("/organization_places", api.GetOrganizationPlaces)
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {})
+	http.HandleFunc("/favicon.ico", func(writer http.ResponseWriter, request *http.Request) {})
 
 	http.ListenAndServe(":8080", nil)
 }
