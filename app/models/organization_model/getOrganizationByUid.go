9410ce96a1fc0b74da0058b649bc6a05ba15733a
Владислав Балобанов <balobasta@gmail.com>
2021-02-11 15:05:18 +0200

    feat(organization model)[models]: add GetOrganizationByUID method

--- app/models/organization_model/getOrganizationByUid.go
+++ app/models/organization_model/getOrganizationByUid.go
@@ -1,1 +1,16 @@
 package organization_model
+
+import (
+	"github.com/pkg/errors"
+	"sport_kld/app/models"
+	"sport_kld/database"
+)
+
+func GetOrganizationByUID(uid models.UID) (Organization, error) {
+	var org Organization
+	if err := database.MysqlDB.Get(&org, "select * from organizations where uid=?", uid.String()); err != nil {
+		return Organization{}, errors.WithStack(err)
+	}
+	org.Preprocess()
+	return org, nil
+}
