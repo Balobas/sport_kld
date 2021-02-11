bd9ff73b9c80db7df4896a1e8c52dbc3383a5caf
Владислав Балобанов <balobasta@gmail.com>
2021-02-11 16:07:30 +0200

    feat(organization model)[models]: add GetOrganizationByPlaceUID method

--- app/models/organization_model/getOrganizationByPlaceUid.go
+++ app/models/organization_model/getOrganizationByPlaceUid.go
@@ -1,1 +1,29 @@
 package organization_model
+
+import (
+	"github.com/pkg/errors"
+	"sport_kld/app/models"
+	"sport_kld/database"
+)
+
+func GetOrganizationByPlaceUID(placeUid models.UID) ([]Organization, []error) {
+
+	var orgsUids []models.UID
+	rows, err := database.MysqlDB.Queryx("select (organization_uid) from organization_places where place_uid = ?", placeUid)
+	if err != nil {
+		return []Organization{}, []error{errors.Wrap(err, "cant select organizations uids")}
+	}
+
+	var resultErrors []error
+
+	for rows.Next() {
+		var uid models.UID
+		if err := rows.Scan(&uid); err != nil {
+			resultErrors = append(resultErrors, errors.Wrap(err, "cant scan organization uid"))
+			continue
+		}
+		orgsUids = append(orgsUids, uid)
+	}
+
+	return GetOrganizationsByUIDs(orgsUids)
+}
