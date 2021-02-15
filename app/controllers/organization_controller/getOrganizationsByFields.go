908e71cde3c3bde8b1e0d92a6fb6a7df3c0f6079
Владислав Балобанов <balobasta@gmail.com>
2021-02-15 16:03:19 +0200

    feat(organization controller)[OrganizationController]: add GetOrganizationsByFields method

--- app/controllers/organization_controller/getOrganizationsByFields.go
+++ app/controllers/organization_controller/getOrganizationsByFields.go
@@ -1,1 +1,7 @@
 package organization_controller
+
+import "sport_kld/app/models/organization_model"
+
+func GetOrganizationsByFields(fieldsNames []string, searchString string) ([]organization_model.Organization, []error) {
+	return organization_model.GetOrganizationsByFields(fieldsNames, searchString)
+}
