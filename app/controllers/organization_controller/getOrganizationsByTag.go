d0c51e457a65d7abb20577aff397401c09c6d380
Владислав Балобанов <balobasta@gmail.com>
2021-02-15 16:03:53 +0200

    feat(organization controller)[OrganizationController]: add GetOrganizationsByTag method

--- app/controllers/organization_controller/getOrganizationsByTag.go
+++ app/controllers/organization_controller/getOrganizationsByTag.go
@@ -1,1 +1,7 @@
 package organization_controller
+
+import "sport_kld/app/models/organization_model"
+
+func GetOrganizationsByTags(searchString string) ([]organization_model.Organization, []error) {
+	return organization_model.GetOrganizationsByTags(searchString)
+}
