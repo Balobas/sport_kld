6b2ff5245f6e8e429b07b88a8dd699e0e3da451d
Владислав Балобанов <balobasta@gmail.com>
2021-02-17 16:10:40 +0200

    fix(): db connection url

--- database/mySql.go
+++ database/mySql.go
@@ -10,7 +10,7 @@ var MysqlDB *sqlx.DB
 
 func init() {
 	var  err error
-	MysqlDB, err = sqlx.Connect("mysql", "b9b59bf12bb030:3e741152@tcp(eu-cdbr-west-03.cleardb.net:3306)/heroku_e9281e77bbf34e2?reconnect=true")
+	MysqlDB, err = sqlx.Connect("mysql", "b9b59bf12bb030:3e741152@tcp(eu-cdbr-west-03.cleardb.net:3306)/heroku_e9281e77bbf34e2")
 	if err != nil {
 		fmt.Println(err)
 		return
