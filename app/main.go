2e22c2e6a9b50357869340a9d7bd30f729e4e607
Владислав Балобанов <balobasta@gmail.com>
2021-02-17 17:15:20 +0200

    refactor main.go : delete commits

--- app/main.go
+++ app/main.go
@@ -9,21 +9,6 @@ import (
 )
 
 func main() {
-	//http.HandleFunc("/place", api.GetPlaceByUID)
-	//http.HandleFunc("/places", api.GetPlacesByUIDs)
-	//http.HandleFunc("/places_by_fields", api.GetPlacesByFields)
-	//http.HandleFunc("/organization_places", api.GetOrganizationPlaces)
-	//http.HandleFunc("/places_by_tag", api.GetPlacesByTag)
-	//
-	//http.HandleFunc("/organization", api.GetOrganizationByUID)
-	//http.HandleFunc("/organizations", api.GetOrganizationsByUIDs)
-	//http.HandleFunc("/organizations_by_fields", api.GetOrganizationsByFields)
-	//http.HandleFunc("/place_organization", api.GetPlaceOrganization)
-	//http.HandleFunc("/organizations_by_tag", api.GetOrganizationsByTag)
-	//
-	//http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {writer.Write([]byte("//////"))})
-	//http.HandleFunc("/favicon.ico", func(writer http.ResponseWriter, request *http.Request) {writer.Write([]byte("favicon ico"))})
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -49,6 +34,4 @@ func main() {
 	router.GET("/organizations_by_tag", gin.WrapF(api.GetOrganizationsByTag))
 
 	router.Run(":" + port)
-
-	//http.ListenAndServe(":8080", nil)
 }
