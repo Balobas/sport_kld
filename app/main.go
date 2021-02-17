b041bfccb5eeac4efdc13f2f057b22e335056fc7
Владислав Балобанов <balobasta@gmail.com>
2021-02-17 16:44:46 +0200

    heroku expirements

--- app/main.go
+++ app/main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
-	"sport_kld/app/api"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/place", api.GetPlaceByUID)
-	http.HandleFunc("/places", api.GetPlacesByUIDs)
-	http.HandleFunc("/places_by_fields", api.GetPlacesByFields)
-	http.HandleFunc("/organization_places", api.GetOrganizationPlaces)
-	http.HandleFunc("/places_by_tag", api.GetPlacesByTag)
+	//http.HandleFunc("/place", api.GetPlaceByUID)
+	//http.HandleFunc("/places", api.GetPlacesByUIDs)
+	//http.HandleFunc("/places_by_fields", api.GetPlacesByFields)
+	//http.HandleFunc("/organization_places", api.GetOrganizationPlaces)
+	//http.HandleFunc("/places_by_tag", api.GetPlacesByTag)
+	//
+	//http.HandleFunc("/organization", api.GetOrganizationByUID)
+	//http.HandleFunc("/organizations", api.GetOrganizationsByUIDs)
+	//http.HandleFunc("/organizations_by_fields", api.GetOrganizationsByFields)
+	//http.HandleFunc("/place_organization", api.GetPlaceOrganization)
+	//http.HandleFunc("/organizations_by_tag", api.GetOrganizationsByTag)
+	//
+	//http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {writer.Write([]byte("//////"))})
+	//http.HandleFunc("/favicon.ico", func(writer http.ResponseWriter, request *http.Request) {writer.Write([]byte("favicon ico"))})
 
-	http.HandleFunc("/organization", api.GetOrganizationByUID)
-	http.HandleFunc("/organizations", api.GetOrganizationsByUIDs)
-	http.HandleFunc("/organizations_by_fields", api.GetOrganizationsByFields)
-	http.HandleFunc("/place_organization", api.GetPlaceOrganization)
-	http.HandleFunc("/organizations_by_tag", api.GetOrganizationsByTag)
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {writer.Write([]byte("//////"))})
-	http.HandleFunc("/favicon.ico", func(writer http.ResponseWriter, request *http.Request) {writer.Write([]byte("favicon ico"))})
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("<html><body>hello</body></html>")
+	router.GET("/", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "", nil)
+	})
 
-	http.ListenAndServe(":8080", nil)
+	router.Run(":" + port)
+
+	//http.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module sport_kld
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jmoiron/sqlx v1.3.1
 	github.com/pkg/errors v0.9.1
