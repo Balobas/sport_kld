7ac6a5847019c49aeb808d39e357956ccbbec7fc
Владислав Балобанов <balobasta@gmail.com>
2021-02-17 15:45:07 +0200

    feat(): add heroku cleardb path

--- app/settings/database.go
+++ app/settings/database.go
@@ -2,6 +2,7 @@ package settings
 
 
 const (
+	DB_URL = "CLEARDB_DATABASE_URL: mysql://b9b59bf12bb030:3e741152@eu-cdbr-west-03.cleardb.net/heroku_e9281e77bbf34e2?reconnect=true"
 	STORAGE = "MySql"
 	STORAGE_NAME = "sportkld"
 	STORAGE_VERSION = ""
--- database/mySql.go
+++ database/mySql.go
@@ -10,7 +10,7 @@ var MysqlDB *sqlx.DB
 
 func init() {
 	var  err error
-	MysqlDB, err = sqlx.Connect("mysql", "root:@tcp(localhost:3306)/sport_kld")
+	MysqlDB, err = sqlx.Connect("mysql", "b9b59bf12bb030:3e741152@eu-cdbr-west-03.cleardb.net/heroku_e9281e77bbf34e2?reconnect=true")
 	if err != nil {
 		fmt.Println(err)
 		return
