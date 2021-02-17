9e138c51a09e3398643fc1096217f3869bda22b2
Владислав Балобанов <balobasta@gmail.com>
2021-02-17 17:14:41 +0200

    feat(main.go): use gin router

--- app/main.go
+++ app/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sport_kld/app/api"
 )
 
 func main() {
@@ -30,9 +31,22 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+
 	router.GET("/", func(context *gin.Context) {
-		context.JSONP(http.StatusOK, "{lol:kek, kek:lol}")
+		context.JSONP(http.StatusOK, "")
 	})
+
+	router.GET("/place", gin.WrapF(api.GetPlaceByUID))
+	router.GET("/places", gin.WrapF(api.GetPlacesByUIDs))
+	router.GET("/places_by_fields", gin.WrapF(api.GetPlacesByFields))
+	router.GET("/organization_places", gin.WrapF(api.GetOrganizationPlaces))
+	router.GET("/places_by_tag", gin.WrapF(api.GetPlacesByTag))
+
+	router.GET("/organization", gin.WrapF(api.GetOrganizationByUID))
+	router.GET("/organizations", gin.WrapF(api.GetOrganizationsByUIDs))
+	router.GET("/organizations_by_fields", gin.WrapF(api.GetOrganizationsByFields))
+	router.GET("/place_organization", gin.WrapF(api.GetPlaceOrganization))
+	router.GET("/organizations_by_tag", gin.WrapF(api.GetOrganizationsByTag))
 
 	router.Run(":" + port)
 
