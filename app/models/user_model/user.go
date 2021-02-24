c91093a7d0bd7baa99b5e56d3d4fa8ef4bfbcccd
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 10:55:57 +0200

    feat(user model)[models]: refactor user struct

--- app/models/user_model/user.go
+++ app/models/user_model/user.go
@@ -3,8 +3,8 @@ package user_model
 import "sport_kld/app/models"
 
 type User struct {
-	UID models.UID
-	Name string
-	Login string
-
+	UID      models.UID `json:"uid" db:"uid"`
+	Name     string     `json:"name" db:"name"`
+	Login    string     `json:"login" db:"login"`
+	Password string     `json:"password" db:"password"`
 }
