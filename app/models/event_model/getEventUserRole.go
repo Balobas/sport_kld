c709fcc7c84597aa73b9d307880af945442c7176
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 16:24:24 +0200

    feat(event model)[models]: add getEventUserRole method

--- app/models/event_model/getEventUserRole.go
+++ app/models/event_model/getEventUserRole.go
@@ -1,1 +1,15 @@
 package event_model
+
+import (
+	"github.com/pkg/errors"
+	"sport_kld/app/models"
+	"sport_kld/database"
+)
+
+func GetEventUserRoleByUid(eventUid, userUid models.UID) (EventUserRole, error) {
+	var role EventUserRole
+	if err := database.MysqlDB.Get(&role, "select * from event_user_roles where userUid=? and eventUid=?", userUid, eventUid); err != nil {
+		return EventUserRole{}, errors.Wrap(err, "cant get user role")
+	}
+	return role, nil
+}
