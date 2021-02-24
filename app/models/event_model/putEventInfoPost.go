f62044dfc9cca90fc13f7a33da02b3bc6ee4fb27
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 16:26:15 +0200

    feat(event model)[models]: add putEventInfoPost method

--- app/models/event_model/putEventInfoPost.go
+++ app/models/event_model/putEventInfoPost.go
@@ -1,1 +1,46 @@
 package event_model
+
+import (
+	"errors"
+	uuid "github.com/satori/go.uuid"
+	"sport_kld/app/models"
+	"sport_kld/database"
+)
+
+func PutEventInfoPost(post EventInfoPost) (models.UID, error) {
+	event, err := GetEventByUid(post.EventUID)
+	if err != nil {
+		return "", err
+	}
+	if event.CreatorUID != post.AuthorUID {
+		return "", errors.New("access denied")
+	}
+
+	if len(post.Text) == 0 {
+		return "", errors.New("post text is empty")
+	}
+
+	if len(post.UID) == 0 {
+		post.UID = models.UID(uuid.NewV1().String())
+		/*
+			post.CreatedTime =
+			post.UpdatedTime =
+		*/
+		_, err := database.MysqlDB.NamedExec("INSERT into event_posts(uid, eventUid, authorUid, text, createdTime, updatedTime) VALUES (:uid, :eventUid, :authorUid, :text, :createdTime, :updatedTime)", &post)
+		if err != nil {
+			return "", err
+		}
+
+		return post.UID, nil
+	}
+
+	/*
+		post.UpdatedTime =
+	*/
+
+	if _, err := database.MysqlDB.NamedExec("update event_posts set text = :text where uid = :uid", &post); err != nil {
+		return "", err
+	}
+
+	return post.UID, nil
+}
