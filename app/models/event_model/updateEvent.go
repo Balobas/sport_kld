eaf8abb76e0d2e10ccef2ae2a0a7a3ece63beffe
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 16:21:16 +0200

    feat(event model)[models]: add updateEvent method

--- app/models/event_model/updateEvent.go
+++ app/models/event_model/updateEvent.go
@@ -1,1 +1,31 @@
 package event_model
+
+import (
+	"github.com/pkg/errors"
+	"sport_kld/app/models"
+)
+
+func UpdateEvent(event Event, executorUid models.UID) error {
+	if err := event.validate(); err != nil {
+		return errors.Wrap(err, " cant update event")
+	}
+
+	oldEvent, err := GetEventByUid(event.UID)
+	if err != nil {
+		return err
+	}
+
+	if oldEvent.CreatorUID != executorUid {
+		return errors.New("cant update event: access denied")
+	}
+
+	oldEvent.Name = event.Name
+	oldEvent.Description = event.Description
+	oldEvent.VisitorsLimit = event.VisitorsLimit
+	oldEvent.Dates = event.Dates
+	oldEvent.Time = event.Time
+	oldEvent.EventPassword = event.EventPassword
+	oldEvent.PlaceUID = event.PlaceUID
+
+	return putEvent(oldEvent)
+}
