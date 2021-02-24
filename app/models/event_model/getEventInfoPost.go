3dd1f31a110973815d16962347a27882391f39e5
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 16:25:48 +0200

    feat(event model)[models]: add getEventInfoPost method

--- app/models/event_model/getEventInfoPost.go
+++ app/models/event_model/getEventInfoPost.go
@@ -1,1 +1,14 @@
 package event_model
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/database"
+)
+
+func GetEventInfoPost(postUid models.UID) (EventInfoPost, error) {
+	var post EventInfoPost
+	if err := database.MysqlDB.Get(&post, "select * from event_posts where uid = ?", postUid); err != nil {
+		return EventInfoPost{}, err
+	}
+	return post, nil
+}
