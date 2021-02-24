8ee5683c03a8bedecd9e9a208b0cc5b8477b6aa0
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 16:22:45 +0200

    feat(event model)[models]: add PutEventUserRole method

--- app/models/event_model/putEventUserRole.go
+++ app/models/event_model/putEventUserRole.go
@@ -7,6 +7,18 @@ import (
 )
 
 func putEventUserRole(role EventUserRole) error {
+	_, err := GetEventUserRoleByUid(role.EventUID, role.UserUID)
+	if err == nil {
+		if result, err := database.MysqlDB.NamedExec("update event_user_roles set role = :role, roleDescription = :roleDescription where userUid= :userUid and eventUid = :eventUid", &role); err != nil {
+			return errors.New("cant update role")
+		} else {
+			// Заменить на логер
+			a, _ := result.RowsAffected()
+			fmt.Println("inserted ", a, " rows")
+		}
+		return nil
+	}
+
 	if result, err := database.MysqlDB.NamedExec("insert into event_user_roles(userUid, eventUid, role, roleDescription) values (:userUid, :eventUid, :role, :roleDescription)", &role); err != nil {
 		return errors.New("cant create role")
 	} else {
