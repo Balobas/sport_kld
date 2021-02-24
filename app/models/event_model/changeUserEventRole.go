50335b9eae97c145532f4d38165d47343ffcb881
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 16:24:47 +0200

    feat(event model)[models]: add changeEventUserRole method

--- app/models/event_model/changeUserEventRole.go
+++ app/models/event_model/changeUserEventRole.go
@@ -1,1 +1,31 @@
 package event_model
+
+import (
+	"errors"
+	"sport_kld/app/models"
+)
+
+func ChangeUserRole(role EventUserRole, executorUid models.UID) error {
+	oldRole, err := GetEventUserRoleByUid(role.EventUID, role.UserUID)
+	if err != nil {
+		return err
+	}
+
+	event, err := GetEventByUid(role.EventUID)
+	if err != nil {
+		return err
+	}
+
+	if event.CreatorUID != executorUid {
+		return errors.New("only creator can change roles")
+	}
+
+	if event.IsOver {
+		return errors.New("event is over")
+	}
+
+	oldRole.Role = role.Role
+	oldRole.RoleDescription = role.RoleDescription
+
+	return putEventUserRole(oldRole)
+}
