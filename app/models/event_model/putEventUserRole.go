0be5f6124fb81962ce8c0f8af1de9b519706f857
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 12:41:23 +0200

    feat(event model)[models]: add putEventUserRole private method

--- app/models/event_model/putEventUserRole.go
+++ app/models/event_model/putEventUserRole.go
@@ -1,1 +1,18 @@
 package event_model
+
+import (
+	"errors"
+	"fmt"
+	"sport_kld/database"
+)
+
+func putEventUserRole(role EventUserRole) error {
+	if result, err := database.MysqlDB.NamedExec("insert into event_user_roles(userUid, eventUid, role, roleDescription) values (:userUid, :eventUid, :role, :roleDescription)", &role); err != nil {
+		return errors.New("cant create role")
+	} else {
+		// Заменить на логер
+		a, _ := result.RowsAffected()
+		fmt.Println("inserted ", a, " rows")
+	}
+	return nil
+}
