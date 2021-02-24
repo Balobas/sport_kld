b826437c1723b794e07b4df4b714f0715dba811a
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 16:25:17 +0200

    feat(event model)[models]: add deleteUserFromEvent method

--- app/models/event_model/deleteUserFromEvent.go
+++ app/models/event_model/deleteUserFromEvent.go
@@ -1,1 +1,27 @@
 package event_model
+
+import (
+	"errors"
+	"sport_kld/app/models"
+	"sport_kld/database"
+)
+
+func DeleteUserFromEvent(userUid, eventUid, executorUid models.UID) error {
+	event, err := GetEventByUid(eventUid)
+	if err != nil {
+		return err
+	}
+
+	if event.CreatorUID != executorUid && userUid != executorUid {
+		return errors.New("access denied")
+	}
+
+	if  _,err := database.MysqlDB.Exec("Delete from event_users where userUid = ? and eventUid = ?", userUid, eventUid); err != nil {
+		return err
+	}
+	if  _,err := database.MysqlDB.Exec("Delete from event_user_roles where userUid = ? and eventUid = ?", userUid, eventUid); err != nil {
+		return err
+	}
+
+	return nil
+}
