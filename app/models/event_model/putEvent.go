f9db68008e2232508dca9f0dfafb05dbfbe102e5
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 12:40:46 +0200

    feat(event model)[models]: add putEvent private method

--- app/models/event_model/putEvent.go
+++ app/models/event_model/putEvent.go
@@ -1,1 +1,20 @@
 package event_model
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"sport_kld/database"
+)
+
+func putEvent(event Event) error {
+	result, err := database.MysqlDB.NamedExec("INSERT INTO events(uid, name, description, dates, time, visitorsNum, visitorsLimit, placeUid, creatorUid, isPrivate, isOver) VALUES (:uid, :name, :description, :dates, :time, :visitorsNum, :visitorsLimit, :placeUid, :creatorUid, :isPrivate, :isOver)", &event)
+	if err != nil {
+		return errors.New("cant put event")
+	}
+
+	// Заменить на логер
+	a, _ := result.RowsAffected()
+	fmt.Println("inserted ", a, " rows")
+
+	return nil
+}
