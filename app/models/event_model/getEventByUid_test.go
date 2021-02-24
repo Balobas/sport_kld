4ae00aca391ff3aeaedf1e1068ae1de6c5390ede
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 17:40:28 +0200

    feat(event model tests)[tests]: add getEventByUid test

--- app/models/event_model/getEventByUid_test.go
+++ app/models/event_model/getEventByUid_test.go
@@ -1,1 +1,25 @@
 package event_model
+
+import (
+	"sport_kld/app/models"
+	"testing"
+)
+
+func TestGetEventByUid(t *testing.T) {
+	uid := models.UID("123")
+
+	event, err := GetEventByUid(uid)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	t.Log(event)
+
+	event, err = GetEventByUid("789")
+	if err != nil {
+		t.Log(err)
+	}
+
+	t.Log("passed")
+}
