d605b645a64d51641c01b33633e2d3b487642a7b
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 16:20:29 +0200

    feat(event model)[models]: refactor putEvent private method

--- app/models/event_model/putEvent.go
+++ app/models/event_model/putEvent.go
@@ -7,6 +7,15 @@ import (
 )
 
 func putEvent(event Event) error {
+	if _, err := GetEventByUid(event.UID); err == nil {
+		_, err = database.MysqlDB.NamedExec(
+			"update events set name = :name, description = :description, dates = :dates, time = :time," +
+			"visitorsNum = :visitorsNum, visitorsLimit = :visitorsLimit, placeUid = :placeUid, creatorUid = :creatorUid, isPrivate = :isPrivate, isOver = :isOver" +
+			" where uid = :uid", &event)
+
+		return err
+	}
+
 	result, err := database.MysqlDB.NamedExec("INSERT INTO events(uid, name, description, dates, time, visitorsNum, visitorsLimit, placeUid, creatorUid, isPrivate, isOver) VALUES (:uid, :name, :description, :dates, :time, :visitorsNum, :visitorsLimit, :placeUid, :creatorUid, :isPrivate, :isOver)", &event)
 	if err != nil {
 		return errors.New("cant put event")
