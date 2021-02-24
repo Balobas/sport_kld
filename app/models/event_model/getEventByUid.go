83a673ca9974d525f3276ec04b73322bf31b90a6
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 12:42:41 +0200

    feat(event model)[models]: add GetEventByUid method

--- app/models/event_model/getEventByUid.go
+++ app/models/event_model/getEventByUid.go
@@ -1,1 +1,15 @@
 package event_model
+
+import (
+	"errors"
+	"sport_kld/app/models"
+	"sport_kld/database"
+)
+
+func GetEventByUid(uid models.UID) (Event, error) {
+	var event Event
+	if err := database.MysqlDB.Get(&event, "select * from events where uid=?", uid); err != nil {
+		return Event{}, errors.New("cant get event")
+	}
+	return event, nil
+}
