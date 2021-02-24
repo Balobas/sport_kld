e1e3dcd15cc3424571d949bd67f61162648e24ff
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 16:23:27 +0200

    feat(event model)[models]: add ChangeEventPrivateStatus method

--- app/models/event_model/changePrivateStatus.go
+++ app/models/event_model/changePrivateStatus.go
@@ -1,1 +1,19 @@
 package event_model
+
+import (
+	"errors"
+	"sport_kld/app/models"
+)
+
+func ChangePrivateStatus(eventUid models.UID) error {
+	event, err := GetEventByUid(eventUid)
+	if err != nil {
+		return err
+	}
+	if event.IsOver {
+		return errors.New("event is over")
+	}
+
+	event.IsPrivate = !event.IsPrivate
+	return putEvent(event)
+}
