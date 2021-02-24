6fcbf885c28a1bb830225e222e1763fbc54a20ed
Владислав Балобанов <balobasta@gmail.com>
2021-02-24 16:22:09 +0200

    feat(event model)[models]: add DeleteEvent method

--- app/models/event_model/deleteEvent.go
+++ app/models/event_model/deleteEvent.go
@@ -1,1 +1,22 @@
 package event_model
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/database"
+)
+
+func DeleteEvent(eventUid models.UID) error {
+	if _, err := GetEventByUid(eventUid); err != nil {
+		return err
+	}
+	if _, err := database.MysqlDB.Exec("delete from events where uid = ?", eventUid); err != nil {
+		return err
+	}
+	if _, err := database.MysqlDB.Queryx("delete from event_users where eventUid = ?", eventUid); err != nil {
+		return err
+	}
+	if _, err := database.MysqlDB.Queryx("delete from event_user_roles where eventUid = ?", eventUid); err != nil {
+		return err
+	}
+	return nil
+}
