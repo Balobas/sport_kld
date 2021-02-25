35c3e1337624c1ee02c9e9ae7851bd4f4f1978a5
Владислав Балобанов <balobasta@gmail.com>
2021-02-25 16:56:11 +0200

    feat(event model tests)[tests]:add ChangeUserRole test

--- app/models/event_model/changeUserEventRole_test.go
+++ app/models/event_model/changeUserEventRole_test.go
@@ -1,1 +1,17 @@
 package event_model
+
+import "testing"
+
+func TestChangeUserRole(t *testing.T) {
+	if err := ChangeUserRole(EventUserRole{
+		UserUID:         "123123123123",
+		EventUID:        "5f460829-76b7-11eb-8b56-0c9d9244",
+		Role:            "Чорт",
+		RoleDescription: "",
+	}, "123123123123"); err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	t.Log("passed")
+}
