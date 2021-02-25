f0c0dba7c4987a783c7db78038252a3ebfb9bbe8
Владислав Балобанов <balobasta@gmail.com>
2021-02-25 17:15:29 +0200

    feat(event model tests)[tests]: add DeleteUserFromEvent test

--- app/models/event_model/deleteUserFromEvent_test.go
+++ app/models/event_model/deleteUserFromEvent_test.go
@@ -1,1 +1,12 @@
 package event_model
+
+import "testing"
+
+func TestDeleteUserFromEvent(t *testing.T) {
+	if err := DeleteUserFromEvent("12", "123", "12"); err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	t.Log("passed")
+}
