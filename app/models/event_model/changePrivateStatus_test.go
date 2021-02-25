f0054ba3afd1c205786d100a1319783d0e3106ed
Владислав Балобанов <balobasta@gmail.com>
2021-02-25 16:55:27 +0200

    feat(event model tests)[tests]:add ChangeEventPrivateStatus test

--- app/models/event_model/changePrivateStatus_test.go
+++ app/models/event_model/changePrivateStatus_test.go
@@ -1,1 +1,12 @@
 package event_model
+
+import "testing"
+
+func TestChangeEventPrivateStatus(t *testing.T) {
+	if err := ChangeEventPrivateStatus("19191919"); err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	t.Log("passed")
+}
