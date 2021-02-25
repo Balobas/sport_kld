c5653a158d632f3b8a94c87960c92d41b8fad04d
Владислав Балобанов <balobasta@gmail.com>
2021-02-25 17:16:43 +0200

    feat(event model tests)[tests]: add putEventUserRole test

--- app/models/event_model/putEventUserRole_test.go
+++ app/models/event_model/putEventUserRole_test.go
@@ -1,1 +1,17 @@
 package event_model
+
+import "testing"
+
+func TestPutEventUserRole(t *testing.T) {
+	if err := putEventUserRole(EventUserRole{
+		UserUID:         "user",
+		EventUID:        "event",
+		Role:            "asd",
+		RoleDescription: "asda",
+	}); err != nil {
+			t.Log(err)
+			t.FailNow()
+	}
+
+	t.Log("passed")
+}
