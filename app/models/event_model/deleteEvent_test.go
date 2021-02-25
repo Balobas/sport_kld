8e0271db22ba5803a78e6cad19e9007b390a1c1f
Владислав Балобанов <balobasta@gmail.com>
2021-02-25 16:57:26 +0200

    feat(event model tests)[tests]:add DeleteEvent test

--- app/models/event_model/deleteEvent_test.go
+++ app/models/event_model/deleteEvent_test.go
@@ -1,1 +1,12 @@
 package event_model
+
+import "testing"
+
+func TestDeleteEvent(t *testing.T) {
+	if err := DeleteEvent("5f460829-76b7-11eb-8b56-0c9d9244"); err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	t.Log("passed")
+}
