1f1f1184225cc540650604fd17dc1e1e90eea1eb
Владислав Балобанов <balobasta@gmail.com>
2021-02-25 17:31:29 +0200

    feat(event model tests)[tests]: add PutEventInfoPost test

--- app/models/event_model/putEventInfoPost_test.go
+++ app/models/event_model/putEventInfoPost_test.go
@@ -1,1 +1,21 @@
 package event_model
+
+import "testing"
+
+func TestPutEventInfoPost(t *testing.T) {
+	uid, err := PutEventInfoPost(EventInfoPost{
+		UID:         "",
+		EventUID:    "1491b99d-7778-11eb-8979-0c9d9244",
+		AuthorUID:   "ttt",
+		Text:        "lol",
+		CreatedTime: "",
+		UpdatedTime: "",
+	})
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+
+	t.Log(uid)
+	t.Log("passed")
+}
