87fd4f0813b1e4eefe2fcab42abfebbb7bc6256a
Владислав Балобанов <balobasta@gmail.com>
2021-02-26 12:44:05 +0200

    feat[EventController]: add PutEventInfoPost method

--- app/controllers/event_controller/putEventInfoPost.go
+++ app/controllers/event_controller/putEventInfoPost.go
@@ -1,1 +1,8 @@
 package event_controller
+
+import "sport_kld/app/models/event_model"
+
+func PutEventInfoPost(post event_model.EventInfoPost) (string, error) {
+	uid, err := event_model.PutEventInfoPost(post)
+	return string(uid), err
+}
