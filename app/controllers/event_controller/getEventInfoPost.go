68a9bb8f7aa4780a4cb9738d271d3079289922cf
Владислав Балобанов <balobasta@gmail.com>
2021-02-26 12:43:23 +0200

    feat[EventController]: add GetEventInfoPost method

--- app/controllers/event_controller/getEventInfoPost.go
+++ app/controllers/event_controller/getEventInfoPost.go
@@ -1,1 +1,10 @@
 package event_controller
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/app/models/event_model"
+)
+
+func GetEventInfoPost(postUid string) (event_model.EventInfoPost, error) {
+	return event_model.GetEventInfoPost(models.UID(postUid))
+}
