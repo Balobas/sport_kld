9f139f883cfd3ffe520e0b2c9ff5e06e2abbad07
Владислав Балобанов <balobasta@gmail.com>
2021-02-26 12:41:08 +0200

    feat[EventController]: add GetEventUserRole method

--- app/controllers/event_controller/getEventUserRole.go
+++ app/controllers/event_controller/getEventUserRole.go
@@ -1,1 +1,10 @@
 package event_controller
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/app/models/event_model"
+)
+
+func GetEventUserRole(eventUid, userUid string) (event_model.EventUserRole, error) {
+	return event_model.GetEventUserRoleByUid(models.UID(eventUid), models.UID(userUid))
+}
