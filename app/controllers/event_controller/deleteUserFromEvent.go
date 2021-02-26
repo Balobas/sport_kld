aecd96b11e50ef80604804f91deac02a8cf12473
Владислав Балобанов <balobasta@gmail.com>
2021-02-26 12:42:08 +0200

    feat[EventController]: add DeleteUserFromEvent method

--- app/controllers/event_controller/deleteUserFromEvent.go
+++ app/controllers/event_controller/deleteUserFromEvent.go
@@ -1,1 +1,10 @@
 package event_controller
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/app/models/event_model"
+)
+
+func DeleteUserFromEvent(userUid, eventUid, executorUid string) error {
+	return event_model.DeleteUserFromEvent(models.UID(userUid), models.UID(eventUid), models.UID(executorUid))
+}
