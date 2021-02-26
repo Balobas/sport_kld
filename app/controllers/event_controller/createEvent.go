18c784069fd507b7c1c3d8b39ba8041e7ab83e4d
Владислав Балобанов <balobasta@gmail.com>
2021-02-26 12:31:35 +0200

    feat[EventController]: add CreateEvent method

--- app/controllers/event_controller/createEvent.go
+++ app/controllers/event_controller/createEvent.go
@@ -1,1 +1,8 @@
 package event_controller
+
+import "sport_kld/app/models/event_model"
+
+func CreateEvent(event event_model.Event) (string, error) {
+	uid, err := event_model.CreateEvent(event)
+	return string(uid), err
+}
