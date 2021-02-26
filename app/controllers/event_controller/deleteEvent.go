ea0416bddb5898e75c69c3a0428d404c76cb2ea3
Владислав Балобанов <balobasta@gmail.com>
2021-02-26 12:31:50 +0200

    feat[EventController]: add DeleteEvent method

--- app/controllers/event_controller/deleteEvent.go
+++ app/controllers/event_controller/deleteEvent.go
@@ -1,1 +1,10 @@
 package event_controller
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/app/models/event_model"
+)
+
+func DeleteEvent(eventUid string) error {
+	return event_model.DeleteEvent(models.UID(eventUid))
+}
