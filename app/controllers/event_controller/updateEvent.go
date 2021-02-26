c68892ef8fad73a980e46065ae2dca55dfef0a76
Владислав Балобанов <balobasta@gmail.com>
2021-02-26 12:40:32 +0200

    feat[EventController]: add UpdateEvent method

--- app/controllers/event_controller/updateEvent.go
+++ app/controllers/event_controller/updateEvent.go
@@ -1,1 +1,10 @@
 package event_controller
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/app/models/event_model"
+)
+
+func UpdateEvent(event event_model.Event, executorUid string) error {
+	return event_model.UpdateEvent(event, models.UID(executorUid))
+}
