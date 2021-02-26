c631291d40ec4901c319f82cd02fad18504e2db6
Владислав Балобанов <balobasta@gmail.com>
2021-02-26 12:32:07 +0200

    feat[EventController]: add JoinEvent method

--- app/controllers/event_controller/joinEvent.go
+++ app/controllers/event_controller/joinEvent.go
@@ -1,1 +1,11 @@
 package event_controller
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/app/models/event_model"
+	"sport_kld/app/models/user_model"
+)
+
+func JoinEvent(user user_model.User, eventUid, password string) error {
+	return event_model.JoinEvent(user, models.UID(eventUid), password)
+}
