9858d99d2a3620ce3a9a1362d21495bbcd6690a2
Владислав Балобанов <balobasta@gmail.com>
2021-02-26 12:34:34 +0200

    feat[EventController]: add getEventByUid method

--- app/controllers/event_controller/getEventByUid.go
+++ app/controllers/event_controller/getEventByUid.go
@@ -1,1 +1,10 @@
 package event_controller
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/app/models/event_model"
+)
+
+func GetEventByUid(uid string) (event_model.Event, error) {
+	return event_model.GetEventByUid(models.UID(uid))
+}
