a8fe470c087472fe8d9643efcc942dad21574f44
Владислав Балобанов <balobasta@gmail.com>
2021-02-26 12:41:41 +0200

    feat[EventController]: add ChangeUserEventRole method

--- app/controllers/event_controller/changeUserEventRole.go
+++ app/controllers/event_controller/changeUserEventRole.go
@@ -1,1 +1,10 @@
 package event_controller
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/app/models/event_model"
+)
+
+func ChangeUserEventRole(role event_model.EventUserRole, executorUid string) error {
+	return event_model.ChangeUserRole(role, models.UID(executorUid))
+}
