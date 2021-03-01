1e53590e4872a0857c8997255148e5d2a15e5ba2
Владислав Балобанов <balobasta@gmail.com>
2021-03-01 17:23:23 +0200

    feat(user model)[models]: add sign in func (jwt)

--- app/models/user_model/signIn.go
+++ app/models/user_model/signIn.go
@@ -1,41 +1,32 @@
-package auth
+package user_model
 
 import (
-	"context"
-	"crypto/sha1"
-	"fmt"
 	"github.com/dgrijalva/jwt-go/v4"
-	"sport_kld/app/models/user_model"
 	"sport_kld/app/settings"
+	"time"
 )
 
-type UserParams struct {
-	Login string
-	Password string
+type Claims struct {
+	jwt.StandardClaims
+	UserUid string
 }
 
-func SignIn(userParams UserParams) (string, error) {
-
-	pwd := sha1.New()
-	pwd.Write([]byte(userParams.Password))
-	pwd.Write([]byte("saaaaalt"))
-
-	password := fmt.Sprintf("%x", pwd.Sum(nil))
-
-	if _, err := user_model.GetUserByLoginAndPassword(userParams.Login, password); err != nil {
+func (user *User) SignIn() (string, error) {
+	oldUser, err := GetUserByLoginAndPassword(user.Login, user.Password)
+	if err != nil {
 		return "", err
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: ,
+			ExpiresAt: jwt.At(time.Now().Add(10000)),
 			ID:        "",
 			IssuedAt:  nil,
 			Issuer:    "",
 			NotBefore: nil,
 			Subject:   "",
 		},
-		Username:       userParams.Login,
+		UserUid:       oldUser.UID.String(),
 	})
 	return token.SignedString(settings.SIGNING_KEY)
 }
