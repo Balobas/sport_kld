95f49b2812c30ce553620175bb7ccf43ba279201
Владислав Балобанов <balobasta@gmail.com>
2021-03-01 15:54:29 +0200

    feat(user model)[models]: add GetUserByLogin method

--- app/models/user_model/getUserByLogin.go
+++ app/models/user_model/getUserByLogin.go
@@ -1,1 +1,11 @@
 package user_model
+
+import "sport_kld/database"
+
+func GetUserByLogin(login string) (User, error) {
+	var user User
+	if err := database.MysqlDB.Get(&user, "select * from users where login=?", login); err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
