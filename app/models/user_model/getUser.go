ac63f14e61adc1bae2964ded6583d4fa9c348dfb
Владислав Балобанов <balobasta@gmail.com>
2021-03-01 15:53:40 +0200

    feat(user model)[models]: add GetUser method

--- app/models/user_model/getUser.go
+++ app/models/user_model/getUser.go
@@ -1,1 +1,14 @@
 package user_model
+
+import (
+	"sport_kld/app/models"
+	"sport_kld/database"
+)
+
+func GetUserByUid(uid models.UID) (User, error) {
+	var user User
+	if err := database.MysqlDB.Get(&user, "select * from users where uid=?", uid); err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
