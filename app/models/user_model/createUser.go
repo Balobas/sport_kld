0c3c8da5e8f319e914501101dae799e4f849f8ed
Владислав Балобанов <balobasta@gmail.com>
2021-03-01 17:27:15 +0200

    feat(user model)[models]: add jwt token generation to CreateUser method

--- app/models/user_model/createUser.go
+++ app/models/user_model/createUser.go
@@ -7,23 +7,33 @@ import (
 	"strings"
 )
 
-func CreateUser(user User) (models.UID, error) {
+func CreateUser(user User) (models.UID, string, error) {
 	//валидация
 	if err := user.validate(); err != nil {
-		return "", errors.Wrap(err, "cant create user")
+		return "", "", errors.Wrap(err, "cant create user")
 	}
 
 	// проверка занят ли логин
 	_, err := GetUserByLogin(user.Login)
 	if err == nil {
-		return "", errors.New("login exist")
+		return "", "", errors.New("login exist")
 	} else if !strings.Contains(err.Error(), "sql: no rows in result set") {
-		return "", err
+		return "", "", err
 	}
 
 	// генерация uid
 	user.UID = models.UID(uuid.NewV1().String())
 
+	if err := putUser(user); err != nil {
+		return "", "", errors.Wrap(err, "cant create user")
+	}
+
+	//генерация jwt токена
+	token, err := user.SignIn()
+	if err != nil {
+		return "", "", err
+	}
+
 	//добавление
-	return user.UID, putUser(user)
+	return user.UID, token, putUser(user)
 }
