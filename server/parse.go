56219aa5692bb227cdfbfffe9af1e6f2349b035f
Владислав Балобанов <balobasta@gmail.com>
2021-03-01 16:12:38 +0200

    feat(jwt parser)[server]: add jwt parser

---
+++ server/parse.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/pkg/errors"
+)
+
+type Claims struct {
+	jwt.StandardClaims
+	Username string `json:"username"`
+}
+
+func ParseToken(accessToken string, signingKey []byte) (string, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return signingKey, nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims.Username, nil
+	}
+
+	return "", errors.New("invalid auth token")
+}
