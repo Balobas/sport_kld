835dd76beeb2a1ad66e4f496aabcf9949b8e42ab
Владислав Балобанов <balobasta@gmail.com>
2021-03-01 15:18:50 +0200

    refactor(user model)[models]

--- app/models/user_model/user.go
+++ app/models/user_model/user.go
@@ -6,5 +6,7 @@ type User struct {
 	UID      models.UID `json:"uid" db:"uid"`
 	Name     string     `json:"name" db:"name"`
 	Login    string     `json:"login" db:"login"`
+	Email    string     `json:"email" db:"email"`
+	Age      int64      `json:"age" db:"age"`
 	Password string     `json:"password" db:"password"`
 }
