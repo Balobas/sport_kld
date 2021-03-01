5efe169a6e3ce434054191d62ca763129fa8e256
Владислав Балобанов <balobasta@gmail.com>
2021-03-01 12:02:27 +0200

    fix(event controller): fix and refactor

--- app/controllers/event_controller/changeUserEventRole.go
+++ app/controllers/event_controller/changeUserEventRole.go
@@ -5,6 +5,6 @@ import (
 	"sport_kld/app/models/event_model"
 )
 
-func ChangeUserEventRole(role event_model.EventUserRole, executorUid string) error {
+func ChangeUserEventRole(role event_model.EventUser, executorUid string) error {
 	return event_model.ChangeUserRole(role, models.UID(executorUid))
 }
