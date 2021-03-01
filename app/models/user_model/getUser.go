4019d11de1cc3cca74009aed66307ac6b913ec76
Владислав Балобанов <balobasta@gmail.com>
2021-03-01 17:25:48 +0200

    fix(user model)[models]: set empty password before return (GetUserByUid)

--- app/models/user_model/getUser.go
+++ app/models/user_model/getUser.go
@@ -10,5 +10,6 @@ func GetUserByUid(uid models.UID) (User, error) {
 	if err := database.MysqlDB.Get(&user, "select * from users where uid=?", uid); err != nil {
 		return User{}, err
 	}
+	user.Password = ""
 	return user, nil
 }
