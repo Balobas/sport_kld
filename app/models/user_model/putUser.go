6fd4d9a746821ead6e27650818679649b927e84f
Владислав Балобанов <balobasta@gmail.com>
2021-03-01 15:54:00 +0200

    feat(user model)[models]: add putUser private method

--- app/models/user_model/putUser.go
+++ app/models/user_model/putUser.go
@@ -1,1 +1,17 @@
 package user_model
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"sport_kld/database"
+)
+
+func putUser(user User) error {
+	res, err := database.MysqlDB.NamedExec("INSERT INTO users(uid, name, login, email, age, password) VALUES (:uid, :name, :login, :email, :age, :password)", &user)
+	if err != nil {
+		return errors.Wrap(err, "cant insert user")
+	}
+	a, _ := res.RowsAffected()
+	fmt.Println("inserted ", a, " rows")
+	return nil
+}
