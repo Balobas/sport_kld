10dbad22670e8703ba56fa637fc87cf4c333b3aa
Владислав Балобанов <balobasta@gmail.com>
2021-03-01 11:03:05 +0200

    feat(event controller)[EventController]: change input params in JoinEvent func

--- app/controllers/event_controller/joinEvent.go
+++ app/controllers/event_controller/joinEvent.go
@@ -3,9 +3,8 @@ package event_controller
 import (
 	"sport_kld/app/models"
 	"sport_kld/app/models/event_model"
-	"sport_kld/app/models/user_model"
 )
 
-func JoinEvent(user user_model.User, eventUid, password string) error {
-	return event_model.JoinEvent(user, models.UID(eventUid), password)
+func JoinEvent(userUid models.UID, eventUid, password string) error {
+	return event_model.JoinEvent(userUid, models.UID(eventUid), password)
 }
