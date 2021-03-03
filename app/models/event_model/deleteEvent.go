b0b6d7a86fcfcf651e1f0b98511599eff84dc157
Владислав Балобанов <balobasta@gmail.com>
2021-03-03 15:12:51 +0200

    feat(event model)[models]: add executor uid field to input params

--- app/models/event_model/deleteEvent.go
+++ app/models/event_model/deleteEvent.go
@@ -1,20 +1,22 @@
 package event_model
 
 import (
+	"errors"
 	"sport_kld/app/models"
 	"sport_kld/database"
 )
 
-func DeleteEvent(eventUid models.UID) error {
-	if _, err := GetEventByUid(eventUid); err != nil {
+func DeleteEvent(eventUid, executorUid models.UID) error {
+	event, err := GetEventByUid(eventUid)
+	if err != nil {
 		return err
+	}
+	if event.CreatorUID != executorUid {
+		return errors.New("permission denied")
 	}
 	if _, err := database.MysqlDB.Exec("delete from events where uid = ?", eventUid); err != nil {
 		return err
 	}
-	//if _, err := database.MysqlDB.Queryx("delete from event_users where event_uid = ?", eventUid); err != nil {
-	//	return err
-	//}
 	if _, err := database.MysqlDB.Queryx("delete from event_user_roles where eventUid = ?", eventUid); err != nil {
 		return err
 	}
