63906e91453d37bda14e57795fb9e3e0ad6feeea
Владислав Балобанов <balobasta@gmail.com>
2021-04-30 16:08:42 +0200

    feat(achievemnts): add struct

--- app/models/user/achievement_model/achievement.go
+++ app/models/user/achievement_model/achievement.go
@@ -1,5 +1,11 @@
 package achievement_model
 
-type UserAchievement struct {
+import "sport_kld/app/models"
 
+type UserAchievement struct {
+	UID models.UID `json:"uid"`
+	UserUID models.UID `json:"userUid"`
+	Name string `json:"name"`
+	Description string `json:"description"`
+	AchievementDate string `json:"achievementDate"`
 }
